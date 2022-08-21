ab4d3007ce99ec25ea70cf6778c943c9a9fc4a3b
Joy Muthoni Malanga <joyalphamuthoni@gmail.com>
2022-08-21 20:01:23 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+
+const multiply = num1 * num2;
+const random=Math.floor(Math.random()*10)+1;
+
+const num3 = 44;
+const num4 = 10;
+const mod = num3 % num4;
+
+const num5=11;
+const num6=13;
+const num7=20;
+
+const max = Math.max(num5, num6, num7);
